2a4bcc48f5f44cad29a080914451352ca985f1b7
Alan <me@alan.codes>
2021-06-04 23:00:50 -0400

    A few tweaks to the format of the daily report

--- twilioNotification.js
+++ twilioNotification.js
@@ -10,18 +10,18 @@ exports.notifyOnOutage = function() {
 
 exports.notifyDailyActivity = function(reward24, reward30, priceHNT, rewardScale, gain, status) {
     admins.forEach(function(admin) {
-        var messageToSend = 'Hotspot Status: ' + status + '\n';
+        var messageToSend = 'Hotspot Status: ' + status + '.\n';
 
-        messageToSend += 'Reward Scale: ' + rewardScale + '\n';
-        messageToSend += 'Gain: ' + gain + '\n';
+        messageToSend += 'Reward Scale: ' + rewardScale + '.\n';
+        messageToSend += 'Gain: ' + gain + '.\n';
         
-        messageToSend += '\nDaily Stats:\n'
-        messageToSend += '24-Hour: ' + Math.round(reward24) + '\n';
-        messageToSend += '30-Day: ' + Math.round(reward30);
+        messageToSend += '\nDaily Stats\n'
+        messageToSend += '24-Hour: ' + Math.round(reward24) + '.\n';
+        messageToSend += '30-Day: ' + Math.round(reward30) + '.';
 
         if (!isNaN(priceHNT) && priceHNT > 0) {
-            messageToSend += '\nPrice: $' + priceHNT + '\n\nFULL Value:\n24Hr: $' + Math.round(reward24 * priceHNT * 100) / 100 + '\n30Day: $' + Math.round(reward30 * priceHNT * 100) / 100;
-            messageToSend += '\n\nSHARE Value:\n24Hr: $' + Math.round((reward24 * 0.2) * priceHNT * 100) / 100 + '\n30Day: $' + Math.round((reward30 * 0.2) * priceHNT * 100) / 100;
+            messageToSend += '\nPrice: $' + priceHNT + '.\n\nFull Value:\n24-Hour: $' + Math.round(reward24 * priceHNT * 100) / 100 + '.\n30-Day: $' + Math.round(reward30 * priceHNT * 100) / 100 + '.';
+            messageToSend += '\n\nShare Value\n24-Hour: $' + Math.round((reward24 * 0.2) * priceHNT * 100) / 100 + '.\n30-Day: $' + Math.round((reward30 * 0.2) * priceHNT * 100) / 100 + '.';
         }
         
         twilioClient.sendSms(admin.phoneNumber, messageToSend);
